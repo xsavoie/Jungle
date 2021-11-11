b7e4ac6f2a2f924e233238fe1cd214ac9a943b5b
Xavier Savoie <xaviersavoie@gmail.com>
2021-11-11 18:22:36 +0000

    implemented user signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,15 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  # login & logout routes
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # signup routes
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
