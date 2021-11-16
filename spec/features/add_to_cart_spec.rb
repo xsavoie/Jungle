2cdd5ddf256db016ea0acfc7cfa2bb4cb57b54b2
Xavier Savoie <xaviersavoie@gmail.com>
2021-11-16 23:08:50 +0000

    Created add_to_cart spec

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to product page", type: :feature, js:true do
+RSpec.feature "Adds product to cart", type: :feature, js:true do
   
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -16,17 +16,18 @@ RSpec.feature "Visitor navigates to product page", type: :feature, js:true do
     end
   end
 
-  scenario "They see the product page" do
+  scenario "They add a product to cart" do
 
     visit root_path
 
     within first('.product') do
-    find_link("Details").click
-    sleep 1.2
+    click_button('Add')
     end
+
+    expect(page).to_not have_link 'My Cart (0)'
+    expect(page).to have_link 'My Cart (1)'
 
     save_screenshot
 
   end
 end
-
